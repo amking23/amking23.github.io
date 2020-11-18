a0ee2eb7c48a8cee5dc00623b5792586ad0ccbf3
Anne Marie King <annemarieking@Annes-MacBook-Pro.local>
2020-11-17 22:18:06 -0600

    Updates

--- src/Components/ScrollTo.js
+++ src/Components/ScrollTo.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Button } from "reactstrap";
 import { scrollTo } from "../utility";
 
-export default props => {
+export default (props) => {
   return (
     <div className={`${props.direction} scroll-to`}>
       <Button
         onClick={() => scrollTo(props.id)}
-        className={`next-button${props.hidden ? " hide-next-button" : ""}${props.inverse ? " inverse" : ""}`}
+        className={`next-button${props.hidden ? " hide-next-button" : ""}${
+          props.inverse ? " inverse" : ""
+        }`}
       >
-        {props.direction === "down" ? <i class="material-icons arrow-icon">arrow_downward</i> : <i class="material-icons arrow-icon">arrow_upward</i>}
+        {props.direction === "down" ? (
+          <i className="material-icons arrow-icon">arrow_downward</i>
+        ) : (
+          <i className="material-icons arrow-icon">arrow_upward</i>
+        )}
       </Button>
     </div>
   );
